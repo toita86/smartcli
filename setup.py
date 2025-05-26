d837074e44b9f61fbc4cf7e3db006ebe8c3ef08a
Eduard Brahas <eduardbrhas@outlook.it>
2025-05-26 20:36:33 +0200

    [IMP] Preparations for upload to PyPi

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="smartcli",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["requests"],
     entry_points={
         "console_scripts": [
             "smart-cli=smartcli.cli:cli",
@@ -12,6 +15,8 @@ setup(
     },
     author="Eduard Brahas",
     description="AI-powered terminal assistant using Ollama",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Environment :: Console",
